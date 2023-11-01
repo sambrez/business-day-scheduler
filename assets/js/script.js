3550e20f7735db0c67f4292fbb07d83b584c0a03
Sam Brez <sambreslin02@gmail.com>
2023-11-01 16:34:28 -0400

    call and display of values stored in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
 
+  // establishing variables of each time block
+  let nine = $('#9');
+  let ten = $('#10');
+  let eleven = $('#11');
+  let twelve = $('#12');
+  let thirteen = $('#13');
+  let fourteen = $('#14');
+  let fifteen = $('#15');
+  let sixteen = $('#16');
+  let seventeen = $('#17');
+
+  // click event that saves the user entries into local storage (id as the key, user input as the value)
   let saveBtn = $('.saveBtn');
   $(saveBtn).on('click', function () {
     console.log('test');
@@ -12,14 +21,7 @@ $(function () {
     localStorage.setItem(hour, content);
   });
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
+  // loop and coditional statement that applies the appropriate class to each time block based on the current hour
   let currentHour = dayjs().get('hour');
   let timeBlock = $('.time-block');
   $(timeBlock).each(function () {
@@ -47,17 +49,18 @@ $(function () {
     }
   })
 
-
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
-
-  // TODO: Add code to display the current date in the header of the page.
+// call by key to get values stored in local storage and set as value to corresponding time block textarea
+  let textArea9 = nine.children('textarea').val(localStorage.getItem('9'));
+  let textArea10 = ten.children('textarea').val(localStorage.getItem('10'));
+  let textArea11 = eleven.children('textarea').val(localStorage.getItem('11'));
+  let textArea12 = twelve.children('textarea').val(localStorage.getItem('12'));
+  let textArea13 = thirteen.children('textarea').val(localStorage.getItem('13'));
+  let textArea14 = fourteen.children('textarea').val(localStorage.getItem('14'));
+  let textArea15 = fifteen.children('textarea').val(localStorage.getItem('15'));
+  let textArea16 = sixteen.children('textarea').val(localStorage.getItem('16'));
+  let textArea17 = seventeen.children('textarea').val(localStorage.getItem('17'));
+  
+  // function that displays the current day and date to the top of the page
   let currentDate = dayjs()
   $('#currentDay').text(currentDate.format('dddd, MMM D, YYYY'));
 });
